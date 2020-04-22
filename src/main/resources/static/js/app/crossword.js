308fb65f54893e9a08726f3a71042ed0a012754e
yang <y2gcoder@gmail.com>
2020-04-22 10:41:59 +0900

    해당 문제 누르면 문제 번호랑 설명까지 나옴 / 문제 맞추기 이제 한다.

--- src/main/resources/static/js/app/crossword.js
+++ src/main/resources/static/js/app/crossword.js
@@ -6,6 +6,21 @@ var crossword = {
       /* 초기화 후 만들자 */
       $(".crossword-board").html("");
       _this.paintPuzzle(puzzle);
+
+      $(".crossword-board").on("mouseenter", `input[id^="item-"]`, function () {
+        // var word = $(this).attr("id");
+        // console.log(word);
+        // 누르면
+      });
+
+      $(".crossword-board").on("click", `input[id^="item-"]`, function () {
+        // 내용 떠야지
+        var word = $(this).attr("across-word")
+          ? $(this).attr("across-word")
+          : $(this).attr("down-word");
+
+        _this.showQuiz(word);
+      });
     }
   },
   paintPuzzle: function (puzzle) {
@@ -24,7 +39,7 @@ var crossword = {
     // puzzle 그리기부터
     for (var i = 0; i < matrix.length; i++) {
       for (var j = 0; j < matrix[i].length; j++) {
-        console.log(`${i}, ${j} : ${matrix[i][j]}`);
+        // console.log(`${i}, ${j} : ${matrix[i][j]}`);
 
         // 값이 없을 때
         if (matrix[i][j] == null || matrix[i][j] == "null") {
@@ -63,6 +78,10 @@ var crossword = {
       var x = matrixWord["x"];
       var y = matrixWord["y"];
       var num = i + 1;
+      var word = matrixWord["word"];
+      var length = word.length;
+      var across = matrixWord["across"];
+
       const newLabelText = document.createElement("span");
       newLabelText.setAttribute("class", "cross-board__item-label-text");
       newLabelText.innerText = num;
@@ -73,10 +92,48 @@ var crossword = {
         "class",
         `crossword-board__item-label crossword-board__item-label-grid-row-${x} crossword-board__item-label-grid-col-${y}`
       );
+      newLabelSpan.setAttribute("data-word", word);
       newLabelSpan.appendChild(newLabelText);
       newLabelDiv.appendChild(newLabelSpan);
+
+      // 가로 / 세로에 따라 하기
+      if (across) {
+        for (var j = y; j < y + length; j++) {
+          // item-i-j에 word넣기
+          $(`#item-${x}-${j}`).attr("across-word", word);
+        }
+      } else {
+        for (var j = x; j < x + length; j++) {
+          $(`#item-${j}-${y}`).attr("down-word", word);
+        }
+      }
     }
     crosswordBoard.appendChild(newLabelDiv);
+  },
+  showQuiz: function (word) {
+    // 번호 찾기 / 넣기
+    for (var i = 0; i < puzzle.matrixWords.length; i++) {
+      var matrixWord = puzzle.matrixWords[i];
+      var targetWord = matrixWord["word"];
+      if (targetWord == word) {
+        var across = matrixWord["across"];
+        var isAcross = across == true ? "across" : "down";
+        console.log(isAcross);
+        var num = i + 1;
+
+        $("#quiz-across").html(isAcross);
+        $("#quiz-number").html(num);
+      }
+    }
+    // 설명 찾기 / 넣기
+    var quizzes = puzzle.info.quizzes;
+    for (var i = 0; i < quizzes.length; i++) {
+      var quiz = quizzes[i];
+      if (word == quiz.word) {
+        var desc = quiz.q_desc;
+        $("#quiz-desc").html(desc);
+      }
+    }
   },
 };
 crossword.init();
